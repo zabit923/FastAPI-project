04fb0d530c46704e71bc675a233ab8f6c398802a
zabit <xaclafun1991@gmail.com>
2024-06-30 15:44:40 +0300

    .

--- src/config.py
+++ src/config.py
@@ -12,5 +12,13 @@ DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_PORT = os.getenv('DB_PORT')
 
+
+DB_HOST_TEST = os.getenv('DB_HOST_TEST')
+DB_NAME_TEST = os.getenv('DB_NAME_TEST')
+DB_PASS_TEST = os.getenv('DB_PASS_TEST')
+DB_PORT_TEST = os.getenv('DB_PORT_TEST')
+DB_USER_TEST = os.getenv('DB_USER_TEST')
+
+
 SMTP_USER = os.getenv('SMTP_USER')
 SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
---
+++ tests/test_auth.py
@@ -0,0 +1,6 @@
+
+
+
+
+def test_register():
+    assert 1 == 1
